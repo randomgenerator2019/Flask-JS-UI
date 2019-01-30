b5b6948eb1748da54d847d25bff124ba47a659ae
randomgenerator2019 <joshua.bretag@gmail.com>
2019-01-29 22:00:45 -0800

    first commit new flask javascript app

---
+++ backend/launch.py
@@ -0,0 +1,13 @@
+import os
+
+# Use this for calls to outside libraries and system based functions
+
+
+def system_os_call():
+    '''
+    Simple function to return a basic os call.
+    :return: str() the current working directory
+    '''
+
+    return str(os.getcwd())
+
